118f3c6962eb94fea00add66658ec76a05e955e3
stefcos24 <stefan22bl@gmail.com>
2023-05-26 09:43:51 +0200

    Refactored app folders

--- src/src/settings.py
+++ src/src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tvw^3+%fk^tb5%p38mo0l6_lp9%=disnl7msr$5!93ppo1#7vi'
+SECRET_KEY = 'django-insecure-ba*302y52x3wkxob)+t%4(6os*m5^6^qjb84lu$x)ugh)-6o8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
