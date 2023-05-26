5e994f9e4726b04c048552faf73d8d8b8bc6ca53
stefcos24 <stefan22bl@gmail.com>
2023-05-26 14:13:33 +0200

    Added first template, urls and view for starting

--- src/domain/urls.py
+++ src/domain/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from domain import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('domain.urls'))
+    path('', views.hello_world, name="domain" ),
 ]
--- src/domain/views.py
+++ src/domain/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def hello_world(request):
+    context = {}
+    return render(request, 'domain/home.html', context)
