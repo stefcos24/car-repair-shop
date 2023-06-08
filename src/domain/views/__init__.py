3aea23fd08cc44b6c7c9903f1ff650b2270f870d
Stefan Cosic <stefan.cosic@pythondevcenter.com>
2023-06-08 09:50:14 +0200

    CRM-2 Added example views, connected with urls and test template

---
+++ src/domain/views/__init__.py
@@ -0,0 +1,2 @@
+from base import hello_world
+from person import get_all_persons
---
+++ src/domain/views/person.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from ..models.person import Person
+
+# Create your views here.
+
+
+def get_all_persons(request):
+    persons = Person.objects.all()
+    context = {
+        "persons": persons
+    }
+    return render(request, 'domain/persons.html', context)
