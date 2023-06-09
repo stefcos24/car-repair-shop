11d326a80a2e8d1912aa09e60e11062f8b6a4104
radza22 <37376260+radza22@users.noreply.github.com>
2023-06-09 13:41:16 +0200

    Prvi Komit

--- src/domain/admin.py
+++ src/domain/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models.person import *
+from .models.customer import *
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Customer)
