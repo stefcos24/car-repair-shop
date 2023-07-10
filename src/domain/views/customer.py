01b017da43939ba6b6017b59cd6e64e8eba34aab
stefancosicpdc <stefan.cosic@pythondevcenter.com>
2023-07-10 22:03:19 +0200

    CRM-1 Added form to customer create view

--- src/domain/views/customer.py
+++ src/domain/views/customer.py
@@ -1,6 +1,9 @@
+import datetime
 import uuid
 
 from django.shortcuts import render, redirect
+
+from domain.forms.customer import CustomerForm
 from domain.models.customer import Customer
 
 # Create your views here.
@@ -23,31 +26,41 @@ def customer_details(request, customer_id: uuid.UUID):
 
 
 def create_customer(request):
-    if request.method=="POST":
-        full_name=request.POST.get('full_name')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        address1=request.POST.get('address1')
-        address2=request.POST.get('address2')
-        city=request.POST.get('city')
-        phone_number = request.POST.get('phone_number')
-        email = request.POST.get('email')
-        jib=request.POST.get('jib')
-        pib=request.POST.get('pib')
-        
-        customer=Customer(
-            id=uuid.uuid4(),
-            full_name=full_name,
-            first_name=first_name,
-            last_name=last_name,
-            address1=address1,
-            address2=address2,
-            city=city,
-            phone_number=phone_number,
-            email=email,
-            JIB=jib,
-            PIB=pib
-        )
-        customer.save()
-        return redirect('customers')
+
+    form = CustomerForm(request.POST)
+
+    if request.method == "POST":
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            full_name = f"{first_name} {last_name}"
+            address1 = form.cleaned_data["address1"]
+            address2 = form.cleaned_data["address2"]
+            city = form.cleaned_data["city"]
+            phone_number = form.cleaned_data["phone_number"]
+            email = form.cleaned_data["email"]
+            jib = form.cleaned_data["email"]
+            pib = form.cleaned_data["email"]
+            active = form.cleaned_data["active"]
+            created = datetime.datetime.now()
+            modified = datetime.datetime.now()
+
+            customer = Customer(
+                id=uuid.uuid4(),
+                full_name=full_name,
+                first_name=first_name,
+                last_name=last_name,
+                address1=address1,
+                address2=address2,
+                city=city,
+                phone_number=phone_number,
+                email=email,
+                jib=jib,
+                pib=pib,
+                created=created,
+                modified=modified,
+                active=active
+            )
+            customer.save()
+            return redirect('customers')
     return render(request, 'domain.customers.html')
