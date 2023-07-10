10f086e1bc645121ee27b392cb9063a14e25b055
stefancosicpdc <stefan.cosic@pythondevcenter.com>
2023-07-10 22:04:11 +0200

    CRM-1 Changed created and modified for other models to be similar

--- src/domain/models/payments_items.py
+++ src/domain/models/payments_items.py
@@ -6,8 +6,8 @@ from domain.models import Payment
 class PaymentsItem(models.Model):
     id = models.UUIDField(primary_key=True)
     payment = models.ForeignKey(Payment, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Payment item: {PaymentsItem.id}"
