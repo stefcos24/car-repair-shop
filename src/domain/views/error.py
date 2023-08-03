c4f12562ed295fb1bb9e076a41be4efcf52169ec
stefancosicpdc <stefan.cosic@pythondevcenter.com>
2023-08-03 15:38:36 +0200

    CRM-7 Added 403 error for not auth users

--- src/domain/views/error.py
+++ src/domain/views/error.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 
-def error_404_view(request,exception):
+def error_403_view(request, exception):
+    return render(request, 'domain/403.html', status=403)
+
+
+def error_404_view(request, exception):
     return render(request, 'domain/404.html', status=404)
 
 
