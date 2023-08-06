c2fb0ce45c02ce040cdb35463422acf4672132b1
stefancosicpdc <stefan.cosic@pythondevcenter.com>
2023-08-06 12:40:30 +0200

    CRM-8 Added url tests for base, auth, person and customer

---
+++ src/tests/base/test_urls.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from domain.views import domain_base
+
+
+class TestBaseUrls(SimpleTestCase):
+
+    def test_base_url_is_resolved(self):
+        base_url = reverse('domain')
+        self.assertEquals(resolve(base_url).func, domain_base)
