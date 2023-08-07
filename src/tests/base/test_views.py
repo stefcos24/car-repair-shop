63809e0537e4eae7ae1747430272a0ccf08b860e
stefancosicpdc <stefan.cosic@pythondevcenter.com>
2023-08-07 15:45:52 +0200

    CRM-8 Extended test base views

--- src/tests/base/test_views.py
+++ src/tests/base/test_views.py
@@ -1,23 +1,25 @@
-from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from tests.fixture_builder import FixtureBuilder
+from tests.helpers import DataFixtureHelpers
+
 
 class TestBaseViews(TestCase):
+    fixtures = FixtureBuilder().person_user().build()
 
     def setUp(self):
-        self.user = User.objects.create_user(
-            username="testuser",
-            password="testpass"
-        )
+        self.user = DataFixtureHelpers.get_admin_user()
         self.client = Client()
-        self.client.login(
-            username="testuser",
-            password="testpass"
-        )
+        self.client.force_login(user=self.user)
 
     def test_get_base(self):
         response = self.client.get(reverse("domain"))
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, "domain/index.html")
+
+    def test_get_base_forbidden(self):
+        self.client.logout()
+        response = self.client.get(reverse("domain"))
+        self.assertEquals(response.status_code, 302)
