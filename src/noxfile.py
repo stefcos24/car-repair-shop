a4f7789f934570cd2c39b12510924b8b3baa0dc9
stefancosicpdc <stefan.cosic@pythondevcenter.com>
2023-09-07 18:29:58 +0200

    Fixed generate html report on pytest

--- src/noxfile.py
+++ src/noxfile.py
@@ -11,7 +11,6 @@ def tests(session):
         "-m",
         "pytest",
         "--junitxml=.pytest_report/report.xml",
-        "--html=.pytest_report/report.html",
         "--self-contained-html",
         "--ds=src.settings",
         "--log-cli-level=DEBUG",
